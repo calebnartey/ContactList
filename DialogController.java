ccb7bf51925b147d60524c52bbc65ec81623a720
Caleb <72252764+calebnartey@users.noreply.github.com>
2021-12-12 19:33:41 +0000

    Update DialogController.java

--- DialogController.java
+++ DialogController.java
@@ -3,10 +3,14 @@ package com.example.javafxhello;
 import com.example.javafxhello.datamodel.Contact;
 import com.example.javafxhello.datamodel.ContactData;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class DialogController {
+
+    @FXML
+    private TableView<Contact> contactTableView;
 
     @FXML
     private TextField firstNameField;
@@ -20,7 +24,7 @@ public class DialogController {
     @FXML
     private TextArea notesDescription;
 
-    public Contact processResults(){
+    public Contact processResults() {
         String firstName = firstNameField.getText().trim();
         String lastName = lastNameField.getText().trim();
         String phoneNumber = phoneNumberField.getText().trim();
@@ -30,8 +34,24 @@ public class DialogController {
         ContactData.getInstance().addContact(newContact);
 
         return newContact;
+
+
     }
 
+
+    public void editContact(Contact contact){
+        firstNameField.setText(contact.getFirstName());
+        lastNameField.setText(contact.getLastName());
+        phoneNumberField.setText(contact.getPhoneNumber());
+        notesDescription.setText(contact.getNotes());
+    }
+
+    public void updateContact(Contact contact){
+        contact.setFirstName(firstNameField.getText());
+        contact.setLastName(lastNameField.getText());
+        contact.setPhoneNumber(phoneNumberField.getText());
+        contact.setNotes(notesDescription.getText());
+    }
 
 
 }
